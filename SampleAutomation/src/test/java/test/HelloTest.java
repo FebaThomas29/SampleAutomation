98d18a94e5d2d92fe6d7de4ac2106f6018ed1089
Dell <Dell@vinil>
2024-12-02 07:29:45 +0530

    First commit

---
+++ SampleAutomation/src/test/java/test/HelloTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.Test;
+
+public class HelloTest {
+	@Test
+	public void testMethod()
+	{
+		 System.out.println("Main1 class");
+	}
+
+	
+
+}
---
+++ SampleAutomation/src/test/java/test/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testSecond()
+	{
+		System.out.println("hello");
+	}
+
+}
